e05ed7cd1e78da4b497fba62f2385fffe85ddc54
rayansioud <78489545+rayansioud@users.noreply.github.com>
2021-03-29 10:43:30 -0400

    script

---
+++ Assets/Mission.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Mission
+{
+    public int missionId; //Identifiaction de chaque mission
+    [Space]
+    public bool active; // Savoir si une mission est active
+    public bool missionComplete; // Savoir si une mission est complète
+    [Space]
+    public bool restartOnNextBall; // Si on perd la balle, d'une façon ou d'une autre, on veut recommencer cette mission pour la prochaine balle
+    public bool stopOnBallEnd; // Si la balle s'arrête nous voulons soit la faire continuer ou arrêter complètement le progrès de la mission
+    public bool resetOnComplete; // Lorsque la mission est complète, on veut la réinitialiser pour la faire répéter plus tard
+    [Space]
+    public float timeToComplete; // Mission basé sur le temps (mission qui dure 10 secondes et s'arrête)
+    [Space]
+    public int score; // Nombre de points que nous obtenons pour la mission (faculatif pour certaine mission)
+    public int quantiteToComplete; // Nombre d'actions pour compléter la mission (Ex: frapper le bumper 3x pour compléter la mission)
+    [HideInInspector]public int actuelQuantite; // Quantité d'actions complétées actuellement
+
+    public void ResetMission()
+    {
+        if (resetOnComplete)
+        {
+            active = false;
+            missionComplete = false;
+            actuelQuantite = 0;
+        }
+    }
+
+    public void DeactiverMission()
+    {
+        active = false;
+        actuelQuantite = 0;
+    }
+
+    public void UpdateMission()
+    {
+        if(active && !missionComplete)
+        {
+            actuelQuantite++;
+
+            // Vérifier si la mission est complète
+        }
+    }
+
+    void VérifierMissionComplète()
+    {
+        if(actuelQuantite >= quantiteToComplete)
+        {
+            missionComplete = true;
+            active = false;
+            if(timeToComplete > 0)
+            {
+                // Arrêter minuteur
+                MissionManager.instance.ArreterMinuteur();
+            }
+
+            ResetMission();
+            Debug.Log("Mission Complète");
+        }
+        
+    }
+}
---
+++ Assets/Sensor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    public bool isStarter; // trigger pour démarrer mission
+    public bool isCounter; // compter missions en cours
+    [Space]
+    public int triggerID; // mission Id pour trigger
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isStarter)
+        {
+            MissionManager.instance.StartMission(triggerID);
+        }
+
+        if (isCounter)
+        {
+            MissionManager.instance.UpdateMission(triggerID);
+        }
+    }
+    void OnCollisoinEnter(Collision col)
+    {
+        if (isStarter)
+        {
+            MissionManager.instance.StartMission(triggerID);
+        }
+
+        if (isCounter)
+        {
+            MissionManager.instance.UpdateMission(triggerID);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color32(0, 0, 255, 125);
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(Vector2.zero, Vector3.one);
+    }
+}
