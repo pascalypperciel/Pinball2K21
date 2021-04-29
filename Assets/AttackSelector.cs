c17c1cbb7ff4619f4656d688d828ac60158f1eba
pascalypperciel <78489743+pascalypperciel@users.noreply.github.com>
2021-04-29 01:15:29 -0400

    Ajout Rapport 3

---
+++ Assets/AttackSelector.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackSelector : MonoBehaviour
+{
+    [SerializeField]
+    public Image selector;
+
+    Vector3 initialPos = new Vector3(-410, 222, 0);
+    private int déplacement = 65;
+
+    GameObject magnet;
+    GameObject earthquake;
+    GameObject missile;
+    GameObject missileGuidé;
+    GameObject slime;
+    GameObject hole;
+
+    void Start()
+    {
+        //Préparation du sélecteur
+        selector.rectTransform.anchoredPosition = initialPos;
+
+        //Préparation de tous les obstacles
+        earthquake = GameObject.Find("Earthquake");
+        magnet = GameObject.Find("Aimant");
+        missile = GameObject.Find("RocketLauncher");
+        missileGuidé = GameObject.Find("RocketLauncher");
+        slime = GameObject.Find("SlimeGun");
+        hole = GameObject.Find("HolePuncher");
+        ÉteindreTout();
+        ChangementPosition((int) initialPos.x);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int currentPos = (int) selector.rectTransform.anchoredPosition.x;
+            if(currentPos < -85)
+            {
+                selector.rectTransform.anchoredPosition = new Vector3(currentPos + déplacement, initialPos.y, initialPos.z);
+                ChangementPosition(currentPos + déplacement);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            int currentPos = (int)selector.rectTransform.anchoredPosition.x;
+            if(currentPos > -390)
+            {
+                selector.rectTransform.anchoredPosition = new Vector3(currentPos - déplacement, initialPos.y, initialPos.z);
+                ChangementPosition(currentPos - déplacement);
+            }
+        }
+    }
+
+    void ChangementPosition(int posX)
+    {
+        ÉteindreTout();
+        switch (posX)
+        {
+            case -410:
+                earthquake.GetComponent<TerrainGenerator>().enabled = true;
+                break;
+            case -345:
+                magnet.GetComponent<Magnétisme>().enabled = true;
+                break;
+            case -280:
+                missile.GetComponent<NormalMissiles>().enabled = true;
+                break;
+            case -215:
+                missileGuidé.GetComponent<GuidéeMissile>().enabled = true;
+                break;
+            case -150:
+                slime.GetComponent<ImmobilisationBalle>().enabled = true;
+                break;
+            case -85:
+                hole.GetComponent<ObstacleTrou>().enabled = true;
+                break;
+        }
+    }
+
+    void ÉteindreTout()
+    {
+        //Désactiver scripts
+        earthquake.GetComponent<TerrainGenerator>().enabled = false;
+        magnet.GetComponent<Magnétisme>().enabled = false;
+        missile.GetComponent<NormalMissiles>().enabled = false;
+        missileGuidé.GetComponent<GuidéeMissile>().enabled = false;
+        slime.GetComponent<ImmobilisationBalle>().enabled = false;
+        hole.GetComponent<ObstacleTrou>().enabled = false;
+
+        //Effacer game objects nécessaires
+        GameObject tigeGuide = GameObject.Find("GuideTige");
+        Destroy(tigeGuide);
+        GameObject slimeGuide = GameObject.Find("GuideSlime");
+        Destroy(slimeGuide);
+    }
+}
---
+++ Assets/GuitarHero.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuitarHero : MonoBehaviour
+{
+    public Sprite pointGauche;
+    public Sprite pointHaut;
+    public Sprite pointBas;
+    public Sprite pointDroite;
+
+    public Canvas canvas;
+    public int nbPoints = 10;
+
+    private List<Image> points;
+    void Start()
+    {
+        points = new List<Image>();
+        canvas.enabled = true;
+        for (int i = 0; i < nbPoints; ++i)
+        {
+            Invoke("CréerSéquence", i);
+        }
+        Invoke("LacherBoule", nbPoints);
+    }
+
+    void CréerSéquence()
+    {
+        int nombre = Random.Range(1, 4);
+        switch (nombre)
+        {
+            case 1:
+                points.Add(CréerPoint(pointGauche, 195));
+                break;
+            case 2:
+                points.Add(CréerPoint(pointHaut, 265));
+                break;
+            case 3:
+                points.Add(CréerPoint(pointBas, 335));
+                break;
+            case 4:
+                points.Add(CréerPoint(pointDroite, 405));
+                break;
+        }
+    }
+
+    Image CréerPoint(Sprite couleur, int posX)
+    {
+        //Instanciation et ajustement de variables
+        GameObject pointGO = new GameObject();
+        pointGO.transform.SetParent(canvas.transform);
+        Image point = pointGO.AddComponent<Image>();
+        point.sprite = couleur;
+
+        //Déplacer vers la bonne place initiale et mettre bonne taille
+        point.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, 260, 0);
+        point.GetComponent<RectTransform>().sizeDelta = new Vector2(65, 65);
+
+        //Rigidbody et Collider
+        CircleCollider2D col = pointGO.AddComponent<CircleCollider2D>();
+        Rigidbody2D rb = pointGO.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(0, -500);
+
+        return point;
+    }
+
+    void LacherBoule()
+    {
+        bool détruite = true;
+        for(int i = 0; i < nbPoints; ++i)
+        {
+            if (points[i] != null)
+            {
+                détruite = false;
+            }
+        }
+
+        Rigidbody rb = GameObject.Find("Boule").GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.None;
+        if (détruite)
+        {
+            rb.velocity = transform.forward * 2;
+        }
+    }
+}
