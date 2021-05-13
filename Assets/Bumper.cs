99cf9f16e0b0e4dc1a4554a5636db5913ad3c6d0
pascalypperciel <78489743+pascalypperciel@users.noreply.github.com>
2021-05-13 15:29:01 -0400

    Addition Bumper.cs

---
+++ Assets/Bumper.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bumper : MonoBehaviour
+{
+    [SerializeField] float puissance = 1f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            contact.otherCollider.attachedRigidbody.AddForce(-1 * contact.normal * puissance, ForceMode.Impulse);
+        }
+        if (anim != null)
+        {
+            anim.SetTrigger("activate");
+        }
+    }
+}
